5cc13bf492190e1027462457b14e2424df0777b5
kflansburg <kevin.flansburg@gmail.com>
2015-06-09 00:26:31 -0700

    add function definitions and tentative directory structure for build project

--- xshop/new.py
+++ xshop/new.py
@@ -19,3 +19,29 @@
 #		|- test
 #		|	|- test.py
 #
+#		The structure for a Build Project:
+#
+#		PROJECT_NAME
+#		|-source
+#		|-packages
+#		|-config
+#		|	|-debian
+#		|	|-.pbuilderrc
+#
+
+#
+#	Generates a new directory, `name` with the required
+#	structure for a test project as described above and 
+#	copies any templates in. 
+#
+
+def new_test_project(name):
+	pass
+
+#
+#	Generates a new directory, `name` with the required
+#	structure for a build project as described above and 
+#	copies any templates in. 
+#
+def new_build_project(name):
+	pass
