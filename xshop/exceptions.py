cc796f45d00ff25c77a21bf7ed17b1ccab158dfb
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-06-24 21:47:47 +0000

    Add two new exceptions for the build function to throw

--- xshop/exceptions.py
+++ xshop/exceptions.py
@@ -12,3 +12,21 @@ class DockerError(Exception):
 		self.value = value
 	def __str__(self):
 		return repr(self.value)
+
+#
+#	Thrown when the build fails
+#
+class BuildError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+#
+#	Thrown when lintian returns errors
+#
+class LintianError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
