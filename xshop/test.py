41aeb8946c4349f2d7b79370bccaa1e7da5b0926
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-07-07 22:25:41 +0000

    Move most functionality into classes. There is a TestCase class which keeps track of variables for a particular test 	This class exposes run() which performs the test, building from source first if neccessary
    Additionally, there is Trial, which a set of constant values to templates,
    	a dictionary for independent variables (provide a list for each value),
    	and an installation source.
    
    	This class builds a multidimensional array representing each combination of the input independent variables, and a TestCase for each,
    	It has a method called run which runs every TestCase.
    
    This should allow easy description of experiments via the Trial class

--- xshop/test.py
+++ xshop/test.py
@@ -18,7 +18,9 @@
 #		Return results of test and clean up
 #
 
+import copy
 import logging
+from xshop import build
 from xshop import colors
 from xshop import exceptions
 from xshop import template
@@ -28,120 +30,159 @@ import shutil
 import os
 import re
 
-#
-#	This function assembles the build context for a given 
-#	container. It creates a temporary context, copies in
-#	relevant files and populates the template with values.
-#
-def build_context(name,d):
-	d['container_name']=name
-	
-	# Copy folder and apply template
-	template.copy_and_template('containers/'+name, 'build-tmp/containers/'+name,d)
+TMP_FOLDER='test-tmp'
 
-	# Copy in test folder
-	shutil.copytree('test','build-tmp/containers/'+name+'/test')
-
-#
-#	A rudimentary attempt to parse only the names of 
-#	containers from a docker-compose.yml file without
-#	having to add a yaml parsing library.
-#
-def parse_docker_compose():
-	regex = re.compile("^([\S]+):\n")
-
-	containers = []
-	f = open('docker-compose.yml')
-	for line in f.readlines():
-		m = regex.match(line)
-		if m:	
-			containers.append(m.group(1))
-	return containers
-
-#
-#	This function reads in the docker-compose.yml and uses
-#	build_context() to construct each of the required
-#	contexts.
-#
-def prepare_build(containers,d):
-	# Create temporary compose folder
-	os.mkdir('build-tmp')
-	os.mkdir('build-tmp/containers')
-
-	# Copy docker-compose.yml
-	shutil.copy2('docker-compose.yml','build-tmp/docker-compose.yml')
-
-	# Constuct each context
-	for container in containers:
-		build_context(container,d)
-	
-	# Move into temporary directory
-	os.chdir('build-tmp')
-
-#
-# 	This function reads the docker-compose.yml and cleans
-# 	up any ephemeral contexts, containers, or images that 
-#	may be created during a test
-#
-def clean_build(containers):
-	# Remove temporary compose folder
-	shutil.rmtree('build-tmp')
-
-#
-#	This function is intended to be the main script for i
-#	running a test.
-#
-def run_test(version,install_type):
-	logging.basicConfig(filename='test.log',level=logging.DEBUG)	
-	
-	library = config.Config().get('library')
-	
-	cwd = os.getcwd()
-	
-	try:
-		# Get containers
-		containers = parse_docker_compose()
+class TestCase:
+	def __init__(self,d,source):
+		# Get Project Configuration
+		self.config = config.Config()
+		self.proj_dir = os.getcwd()
+		self.compose = config.parse_docker_compose()
+		self.containers = [c for c in self.compose]
+		self.library=self.config.get('library')
 		
-		# Generate template dictionary	
-		d = {'library':library,
-			'version':version,
-			'install_type':install_type}
+		# Get Test Case Variables
+		self.source = source
+		self.d = d
+		self.pkgdir=self.proj_dir+'/packages/'+self.library+'_'+'_'.join(map(lambda x: d[x], sorted(d.keys())))
+	
+	# Creates Docker context for a given container in the test
+	# environment.
+	def __build_context(self,name):
+		# Create Templating Dictionary
+		templated = copy.deepcopy(self.d)
+		templated['container_name']=name
+		templated['library']=self.library
+		templated['install_type']=self.source
+	
+		# Copy folder and apply template
+		template.copy_and_template('containers/'+name,
+			TMP_FOLDER+'/containers/'+name,
+			templated)
 
-		# Prepare Build
-		logging.info(colors.colors.OKGREEN+"Preparing Build Context."+colors.colors.ENDC)
-		prepare_build(containers,d)
+		# Copy in test folder
+		shutil.copytree('test',TMP_FOLDER+'/containers/'+name+'/test')
 
-		if install_type=='debian':
-			# Copy in package
-			in_path = '../packages/'+library+'-'+version
-			out_path = 'containers/target/'+library+'-'+version
-			shutil.copytree(in_path,out_path)
+		# If install source is debian, copy it into target context	
+		if self.source=='debian' and name=='target':
+			shutil.copytree(self.pkgdir, 
+				TMP_FOLDER+'/containers/target/'+self.library+'-'+templated['version'])
 
-		# Check for base test image	
-		logging.info(colors.colors.OKGREEN+"Rebuilding Base Test Image."+colors.colors.ENDC)
-		dockerw.build_image('base_test_image')
+	# Creates temporary build dir and constructs contexts for
+	# each container in the experiment inside
+	def __prepare_build(self):
+		os.mkdir(TMP_FOLDER)
+		os.mkdir(TMP_FOLDER+'/containers')
 
-		# Run Docker Compose
-		dockerw.compose_up()
+		# Copy docker-compose.yml
+		shutil.copy2('docker-compose.yml',TMP_FOLDER+'/docker-compose.yml')
 
-		# Call hook
-		logging.info(colors.colors.OKGREEN+"Running Hooks:"+colors.colors.ENDC)
-		vuln=False
-		for c in containers:
-			logging.info(colors.colors.OKGREEN+"\t"+c+colors.colors.ENDC)
-			c = "xshop_"+c+"_1"
-			if dockerw.run_hook(c,'run_exploit'):
-				vuln=True
-			# TODO - Change hook to check environment variable
-		
-		logging.info(colors.colors.OKGREEN+"Result: "+str(vuln)+colors.colors.ENDC)
-
-	finally:
-		os.chdir(cwd)
+		# Constuct each context
+		for c in self.containers:
+			self.__build_context(c)
+	
+		# Move into temporary directory
+		os.chdir(TMP_FOLDER)
+	
+	# Removes temporary build directory
+	def __clean(self):
+		# Remove temporary compose folder
+		os.chdir(self.proj_dir)
+		if os.path.isdir(TMP_FOLDER):
+			shutil.rmtree(TMP_FOLDER)
+	
+		# Terminate Test Containers
 		dockerw.compose_down()
-		# Clean up
-		logging.info(colors.colors.OKGREEN+"Cleaning Up."+colors.colors.ENDC)
-		clean_build(containers)
-		logging.info(colors.colors.OKGREEN+"Done."+colors.colors.ENDC)
-			
-	return vuln
+		
+		# End any logging
+		for handler in self.log.handlers[:]:
+			handler.close()
+			self.log.removeHandler(handler)
+
+	# Call exploit hook in each container	
+	def __call_hooks(self):
+		for c in self.containers:
+			logging.info(colors.colors.OKGREEN+"\t"+c+colors.colors.ENDC)
+			container = 'xshop_'+c+'_1'
+			result=dockerw.run_hook(container,'run_exploit')	
+			self.results[c]=result
+			if result['ret']==2:
+				self.vuln=True
+
+	# Runs hooks in test environment
+	def run(self):
+		logging.basicConfig(filename='test.log',level=logging.DEBUG)	
+		self.log=logging.getLogger()
+		self.results = {}
+		self.vuln = False
+		try:
+			# If install type is source, build it
+			if self.source=='source':
+				self.source='debian'
+				build.build(self.d)
+		
+			# Prepare Build
+			logging.info(colors.colors.OKGREEN+"Preparing Build Context."+colors.colors.ENDC)
+			self.__prepare_build()
+
+			# Check for base test image	
+			logging.info(colors.colors.OKGREEN+"Rebuilding Base Test Image."+colors.colors.ENDC)
+			dockerw.build_image('base_test_image')
+
+			# Run Docker Compose
+			dockerw.compose_up()
+
+			# Call hooks in each container, catching any exceptions
+			logging.info(colors.colors.OKGREEN+"Running Hooks:"+colors.colors.ENDC)
+			self.__call_hooks()
+			logging.info(colors.colors.OKGREEN+"Result: "+str(self.vuln)+colors.colors.ENDC)
+
+		finally:
+			logging.info(colors.colors.OKGREEN+"Cleaning Up."+colors.colors.ENDC)
+			# Clean up
+			self.__clean()
+
+		return self.vuln
+
+#
+# 	Decscribes a series of tests with one or more 
+#	independent variables
+#
+class Trial:
+	def __init__(self,cvars,ivars,source):
+		self.ivars = ivars
+		self.cvars = cvars
+		self.source = source
+		
+		self.cases = self.__array_builder({},copy.deepcopy(self.ivars))	
+
+	def __array_builder(self,d,ivars):
+		if ivars=={}:
+			dnew = copy.deepcopy(d)
+			dnew.update(self.cvars)
+			return {'vars':d,'case':TestCase(dnew,self.source)}
+		else:
+			key, value = ivars.popitem()
+			l = []
+			for v in value:
+				dnew = copy.deepcopy(d)
+				dnew[key]=v
+				l.append(self.__array_builder(dnew,copy.deepcopy(ivars)))
+			return l
+	def __run_tests(self,obj):
+		for o in obj:
+			if isinstance(o,list):
+				self.__run_tests(o)
+			else:
+				case = o['case']
+				d = o['vars']
+				print colors.colors.BOLD+"Running Test: "+colors.colors.ENDC+str(d)+", ",
+				if case.run():
+					print colors.colors.FAIL+"Vulnerable"+colors.colors.ENDC
+				else:
+					print colors.colors.OKGREEN+"Invulnerable"+colors.colors.ENDC
+
+	# Runs a test case for each value
+	def run(self):
+		self.__run_tests(self.cases)
+
