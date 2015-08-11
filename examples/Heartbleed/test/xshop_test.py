de95ec289804da655f7c16166f3ee13b136d5340
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-08-11 19:44:51 -0400

    Update to reflect new constants/variables definition

--- examples/Heartbleed/test/xshop_test.py
+++ examples/Heartbleed/test/xshop_test.py
@@ -1,8 +1,10 @@
 import heartbleed
 from subprocess import call as sh
+import os
 
 def run(H):
 	H.run('attacker','run_exploit')
 
 def run_exploit():
+        print os.environ['container']
 	return heartbleed.main()
