5cd6af1100ae05b9c926c6ace8542a54ef05be93
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-08-16 07:10:10 +0000

    Shellshock tests and data

--- examples/Shellshock/3.1.py
+++ examples/Shellshock/3.1.py
@@ -3,8 +3,8 @@ import json
 from xshop import test
 
 #versions = {'3.0':22,'3.1':23,'3.2':53,'4.0':44,'4.1':17,'4.2':53,'4.3':39}
-version = '3.2'
-patches=53
+version = '3.1'
+patches=23
 
 cves = ['6271','6277','6278','7169','7186','7187','LAST']
 
