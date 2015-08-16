1a5cd6ce9e9bd454811c74309483fa2aba98354c
Insu Yun <wuninsu@gmail.com>
2015-08-16 22:04:48 +0000

    change vuln

--- examples/BinCFI/test/vuln.c
+++ examples/BinCFI/test/vuln.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+void wrap_exit() {
+  exit(-1);
+}
 
-void func(int argc, char** argv) {
+void evil() {
+  wrap_exit();
+  system("/bin/sh");
+}
+
+void bof(int argc, char** argv) {
   char buf[0x100];
-  if (argc > 1)
-    strcpy(buf, argv[1]);
+  strcpy(buf, argv[1]);
 }
 
 int main(int argc, char** argv) {
-  func(argc, argv);
+  bof(argc, argv);
 }
