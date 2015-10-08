196753fd796c6f5cba625daea3bdef20b4b639c0
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-10-08 03:17:33 +0000

    Template now only wraps the templating engine, file processing will be done in other modules

--- xshop/template.py
+++ xshop/template.py
@@ -1,70 +1,22 @@
-#
-#	template
-#
-#		This module provides functions for applying
-#		templating to files and folders. The general
-#		behavior is for a dictionary of substitutions
-# 		to be provided where <%= variable %> is
-#		replaced by the value of variable in every
-# 		appearance in the file. The function reports
-#		if any template elements remain in the file.
-#
+"""
+Template Module
+"""
 
 import jinja2
-import os
-import shutil
-import re
 
-def get_env():
-	templateLoader = jinja2.FileSystemLoader( searchpath="." )
-	templateEnv = jinja2.Environment( loader=templateLoader )
-	return templateEnv
+def __get_env(path):
+    templateLoader = jinja2.FileSystemLoader( searchpath=path )
+    templateEnv = jinja2.Environment( loader=templateLoader )
+    return templateEnv
 
-OMIT = '[\s\S]+(.deb$|.tar.gz$|.swp$|.changes$|.dsc$|.tar.xz$|.tar.bz2$)'
-
-def template_file_contents(path,d):
-	e = get_env()
-	template = e.get_template( path )
-	return template.render( d )
-
-#
-#	Replaces substitutions within a file. Destructive
-#
-def template_file(path,d):
-	if re.compile('.*Dockerfile').match(path):
-		template = env.get_template( path )
-		output = template.render( d )
-		os.remove(path)
-		f = open(path,'w')
-		f.write(output)
-		f.close()
-
-#
-#	Traverses a folder and runs template_file on each
-#	file found. Destructive
-#
-def template_folder(path,d):
-	if os.path.isfile(path):
-		# File
-		if not re.compile(OMIT).match(path):
-			template_file(path,d)
-	else:
-		files = os.listdir(path)
-		for f in files:
-			template_folder(path+'/'+f,d)	
-
-#
-#	Accepts a file or folder `path`, copies it to
-#	`output`, and then applies templating in place.
-#
-def copy_and_template(path,output,d):
-	global env
-	env = get_env()	
-	if os.path.isdir(path):
-		# Folder
-		shutil.copytree(path,output)
-		template_folder(output, d)	
-	else:
-		# Single File
-		shutil.copy2(path,output)
-		template_file(output,d)
+def template_container_dockerfile(config, container, template_dict):
+    """
+    Returns the contents of the specified container's Dockerfile,
+    with the dictionary of template values applied.
+    """
+    
+    template_dict.update({'container_name':container})
+    dockerfilepath = "/containers/"+container+"/Dockerfile"
+    Templater = __get_env(config.project_directory)
+    template = Templater.get_template(dockerfilepath)
+    return template.render(template_dict)
