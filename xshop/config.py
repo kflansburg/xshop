fa404598f6b14a6e0ea8a3ce5ba87af4882902e8
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-10-08 03:16:23 +0000

    Modify config to be an object which is created in TestCase, and passed to any other modules, removing repetitive reading of config.yml

--- xshop/config.py
+++ xshop/config.py
@@ -6,73 +6,108 @@
 #		project directory
 #
 
+import copy
 import os
 import yaml
 from xshop import exceptions
+from xshop import randomname
 
-def parse_docker_compose():
-	f = open('docker-compose.yml','r')
-	return yaml.load(f.read())
+def generate_new_config(library):
+    """
+    Generates an empty configuration file
+    """
 
+    config = {
+        "constants":{
+            "build-dependencies": [],
+            "library":library,
+            "install_type":"source",
+            "provider":"docker"},
+        "variables":{"version":[]},
+        "source":[],
+        "public_keys":[],
+        "notes":""
+    }
 
-#
-#	Creates empty config file
-#
-def generate_new_config():
-	config = {}
-	f = open('config.yaml','w')
-	f.write(yaml.dump(config))
-	f.close()
+    f = open('config.yml','w')
+    f.write(yaml.dump(config))
+    f.close()
 
-#
-#	Object which represents a config file and allows
-#	reading and writing files
-#
 class Config:
-	#
-	#	Checks if a config already exists and if
-	#	not creates a new one.
-	#
-	def __init__(self):
-		self.path='config.yaml'
-		if os.path.isfile(self.path):
-			self.load_config()
-		else:
-			raise exceptions.ConfigError("Config not found, are you in a project directory?")	
-	#
-	#	Initializes config as empty dict and saves
-	#	
-	def generate_new_config(self):
-		self.config={}
-		self.persist()
+    """
+    Object for collecting information specific to a project and test case.
+    """
 
-	#
-	#	Saves config dict as json dump
-	# 
-	def persist(self):
-		f = open(self.path,'w')
-		f.write(yaml.dump(self.config))
-		f.close()	
+    project_directory=None
+    build_directory=None  
+    source_directory=None
+    test_directory=None
+ 
+    config={}
 
-	#
-	#	Loads json from file into config dict
-	#
-	def load_config(self):
-		f = open(self.path, 'r')
-		self.config = yaml.load(f.read())
-		f.close()
-		
-	#
-	#	Returns the value of a given key
-	#
-	def get(self,key):
-		return self.config[key]
+    test_vars={}
 
-	#
-	#	Sets the value of a key and persists
-	#
-	def put(self,key,value):
-		self.config[key]=value
-		self.persist()
+    variables={}
 
-		
+    compose={}
+    containers={}
+
+    target=None
+
+    def __init__(self, variables, target):
+
+        self.project_directory = os.getcwd()
+	self.test_directory = self.project_directory+"/test"
+
+        self.variables = variables
+    
+        self.target=target
+
+        self.config = self.__load_file('config.yml')
+
+        self.__verify_parse_config()
+
+        self.compose = self.__load_file('docker-compose.yml')
+        
+        self.__randomize_container_names()
+        
+    def __load_file(self, filename):
+        full_file_path = self.project_directory+"/"+filename
+        if os.path.isfile(full_file_path):
+            f = open(full_file_path)
+            return yaml.load(f.read())
+        else:
+            raise exceptions.ConfigError("%s not found"%(filename,))
+
+    def __randomize_container_names(self):
+        self.containers={}
+        for c in self.compose:
+            build_files_directory = self.project_directory+"/containers/"+c
+            self.containers[c] = {'alias':randomname.generate(),
+                'build_files_directory':build_files_directory}
+            
+
+    def __verify_parse_config(self):
+        ## Optional Stuff
+        if not 'constants' in self.config:
+            self.config['constants'] = {}
+       
+        self.test_vars = copy.deepcopy(self.config['constants'])
+        self.test_vars.update(self.variables)
+    
+        ## Required Stuff
+        if not 'provider' in self.test_vars:
+            raise exceptions.ConfigError("No provider found!")
+
+        if not 'install_type' in self.test_vars:
+            raise exceptions.ConfigError("No install type found!")
+
+        if self.test_vars['install_type']=='source':
+            if not 'version' in self.test_vars:
+                raise exceptions.ConfigError("No version for source")
+            if not 'library' in self.test_vars:
+                raise exceptions.ConfigError("No library found for source")
+            self.source_path = (self.project_directory + 
+                "/source/%s-%s.tar.gz")%(self.test_vars['library'],
+                    self.test_vars['version'],)
+
