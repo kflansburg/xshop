520eebb5fb9b81b73e2c0628c5ec5b50840fa9c0
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-10-11 18:44:01 +0000

    Logs while command runs, not just at the end

--- xshop/sh.py
+++ xshop/sh.py
@@ -3,6 +3,7 @@ sh module
 
 Allows running of shell commands, with proper logging.
 """
+
 import logging
 import subprocess
 
@@ -15,9 +16,17 @@ def run(command, shell=False):
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         shell=shell)
-    
+
+    output = ""
+    while process.poll() is None:
+        line=process.stdout.readline()
+        logging.debug(line)
+        output+=line
+
     stdout,stderr = process.communicate()
     logging.debug(stdout)
+    
+    stdout = output+stdout
     logging.error(stderr)
     
     return {'return_code':process.returncode,'stdout':stdout,'stderr':stderr}
