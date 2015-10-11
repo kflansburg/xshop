efaa6e17700257acbd826b0ed6031a68eb3acea0
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-10-11 18:45:12 +0000

    Reformat and make some methods private

--- xshop/pull.py
+++ xshop/pull.py
@@ -1,16 +1,19 @@
-#
-#   Pull
-#
-#       Automatic downloading of source files based on config.yaml
-#
-#
+"""
+Pull Module
+
+    Automatic downloading of source files based on config.yaml
+"""
 
 from xshop import config
 import re
 from subprocess import call as sh
 import os
 from xshop import colors
-def generate_urls(urls):
+
+def __generate_urls(urls):
+    """
+    Expands URL lists.
+    """
     REGEX = '([^{}]+)({.+})([^{}]*)'
     r = re.compile(REGEX)
     results = []    
@@ -26,74 +29,167 @@ def generate_urls(urls):
             results.append(url)
     return results
 
-def download(url):
+def __download(url):
+    """
+    Downloads a given URL and attempts to verify it.
+    """
     print colors.colors.BOLD+"Downloading:\t%s"%(url)+colors.colors.ENDC
     devnull=open(os.devnull,'w')
+    
     # Attempt to download file
     if sh(['wget','-N',url],stderr=devnull):
         print colors.colors.FAIL+"FAILED"+colors.colors.ENDC
         return 1
+    
     else:
         print colors.colors.OKGREEN+"Success"+colors.colors.ENDC
+    
         # Attempt to download .asc
-        if sh(['wget','-N',url+".asc"],stderr=devnull,stdout=devnull) and sh(['wget','-N',url+".sig"],stderr=devnull,stdout=devnull) and sh(['wget','-N',url+".sig.asc"],stderr=devnull,stdout=devnull):
-            print colors.colors.WARNING+"No signing file found, looking for hashes."+colors.colors.ENDC
+        if (sh(['wget',
+                '-N',
+                url+".asc"],
+                stderr=devnull,
+                stdout=devnull) and 
+            sh(['wget',
+                '-N',
+                url+".sig"],
+                stderr=devnull,
+                stdout=devnull) and 
+            sh(['wget',
+                '-N',
+                url+".sig.asc"],
+                stderr=devnull,
+                stdout=devnull)):
+            
+            print (colors.colors.WARNING
+                    + "No signing file found, looking for hashes."
+                    + colors.colors.ENDC)
+            
             f = url.split('/')[-1]
             folder = '/'.join(url.split('/')[:-1])+"/CHECKSUMS"
-            if sh(['wget','-N',url+".md5"],stderr=devnull,stdout=devnull) and sh('curl '+folder+' | grep '+f+' > '+f+'.md5',stderr=devnull,stdout=devnull,shell=True):
-                print colors.colors.WARNING+"No MD5"+colors.colors.ENDC 
+            
+            
+            if (sh(['wget',
+                    '-N',
+                    url+".md5"],
+                    stderr=devnull,
+                    stdout=devnull) and 
+                sh('curl '+folder+' | grep '+f+' > '+f+'.md5',
+                    stderr=devnull,
+                    stdout=devnull,shell=True)):
+                
+                print (colors.colors.WARNING
+                    + "No MD5"
+                    + colors.colors.ENDC)
             else:
-                print colors.colors.OKGREEN+"MD5 Found!"+colors.colors.ENDC 
+                
+                print (colors.colors.OKGREEN
+                        + "MD5 Found!"
+                        + colors.colors.ENDC )
                 return 3
-            if sh(['wget','-N',url+".sha1"],stderr=devnull,stdout=devnull):
-                print colors.colors.WARNING+"No SHA1"+colors.colors.ENDC 
+            
+            if sh(['wget',
+                    '-N',
+                    url+".sha1"],
+                    stderr=devnull,
+                    stdout=devnull):
+                print(colors.colors.WARNING
+                    + "No SHA1"
+                    + colors.colors.ENDC )
                 return 5
             else:
-                print colors.colors.OKGREEN+"SHA1 Found!"+colors.colors.ENDC 
+                print (colors.colors.OKGREEN
+                    + "SHA1 Found!"
+                    + colors.colors.ENDC )
                 return 4
 
         else:
-            print colors.colors.OKGREEN+"Signing file found!"+colors.colors.ENDC
+            print (colors.colors.OKGREEN
+                + "Signing file found!"
+                + colors.colors.ENDC)
             return 0 
 
-def verify(u):
+def __verify(u):
+    """
+    Verifies a given file with GPG.
+    """
+
     f = u.split('/')[-1]
     devnull=open(os.devnull,'w')
-    if sh(['gpg','--verify',f+".asc"],stdout=devnull,stderr=devnull) and sh(['gpg','--verify',f+".sig"],stdout=devnull,stderr=devnull):
-        print colors.colors.WARNING+"Verification Failed!"+colors.colors.ENDC
+
+    if (sh(['gpg',
+            '--verify',
+            f+".asc"],
+            stdout=devnull,
+            stderr=devnull) and 
+        sh(['gpg',
+            '--verify',
+            f+".sig"],
+            stdout=devnull,
+            stderr=devnull)):
+
+        print (colors.colors.WARNING
+            + "Verification Failed!"
+            + colors.colors.ENDC)
         return 1
     else:
-        print colors.colors.OKGREEN+"Verified"+colors.colors.ENDC
+        print (colors.colors.OKGREEN
+            + "Verified"
+            + colors.colors.ENDC)
  
-def check_sha1(u):
+def __check_sha1(u):
+    """
+    Checks a file with SHA-1
+    """
+
     f = u.split('/')[-1]+".sha1"
     if sh(['sha1sum','-c',f]):
-        print colors.colors.WARNING+"SHA1 Verification Failed!"+colors.colors.ENDC
+        print(colors.colors.WARNING
+            + "SHA1 Verification Failed!"
+            + colors.colors.ENDC)
         return 1
     else:
-        print colors.colors.OKGREEN+"SHA1 Verified"+colors.colors.ENDC
+        print(colors.colors.OKGREEN
+            + "SHA1 Verified"
+            + colors.colors.ENDC)
 
-def check_md5(u):
+def ___check_md5(u):
+    """
+    Checks a given file with MD5
+    """
+
     f = u.split('/')[-1]+".md5"
+
     if sh(['md5sum','-c',f]):
-        print colors.colors.WARNING+"MD5 Verification Failed!"+colors.colors.ENDC
+        print(colors.colors.WARNING
+            + "MD5 Verification Failed!"
+            + colors.colors.ENDC)
         return 1
     else:
-        print colors.colors.OKGREEN+"MD5 Verified"+colors.colors.ENDC
+        print(colors.colors.OKGREEN
+            + "MD5 Verified"
+            + colors.colors.ENDC)
 
 def pull():
-    c = config.Config()
-    urls = c.get('source')
+    """
+    Pulls files associated with a given project.
+    """
+
+    c = config.Config({'version':'blah'},None)
+
+    urls = c.config['source']
     print "Source URLS:"
-    urls = generate_urls(urls)
+    urls = __generate_urls(urls)
     for u in urls:
         print u
     try:
-        keys = c.get('public_keys')    
+        keys = c.config['public_keys']
         print "Public Keys:"
-        print colors.colors.WARNING+"THESE MUST BE ADDED MANUALLY"+colors.colors.ENDC
+        print(colors.colors.WARNING
+            + "THESE MUST BE ADDED MANUALLY"
+            + colors.colors.ENDC)
 	print "gpg  --keyserver pgp.mit.edu --send-keys <key-id>"
-        for k in c.get('public_keys'):
+        for k in c.config['public_keys']:
             print k
     except KeyError:
         print "No public keys mentioned"
@@ -107,28 +203,32 @@ def pull():
     failed = False
     verifyfail = False
     for u in urls:
-        ret = download(u)
+        ret = __download(u)
         if ret==1:
             failed = True
         elif ret==0:
-            if verify(u):
+            if __verify(u):
                 verifyfail=True
         elif ret==3:
-            if check_md5(u):
+            if __check_md5(u):
                 verifyfail=True
         elif ret==4:
-            if check_sha1(u):
+            if __check_sha1(u):
                 verifyfail=True
     os.chdir(rootdir)
     
 
     if failed:
-        print colors.colors.FAIL+"One or more files could not be retrieved!"+colors.colors.ENDC
+        print(colors.colors.FAIL
+            + "One or more files could not be retrieved!"
+            + colors.colors.ENDC)
     if verifyfail:
-        print colors.colors.WARNING+"One or more of the file verifications failed!"+colors.colors.ENDC
+        print(colors.colors.WARNING
+            + "One or more of the file verifications failed!"
+            + colors.colors.ENDC)
     
     try:
-        notes = c.get('notes')
+        notes = c.config['notes']
         print colors.colors.OKBLUE+notes+colors.colors.ENDC
     except KeyError:
         print colors.colors.OKBLUE+"No Notes."+colors.colors.ENDC
