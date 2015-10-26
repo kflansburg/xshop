7cc08e763960637c3fdd6dccaf34bd2896a265d5
Kevin Flansburg <kevin.flansburg@gmail.com>
2015-10-26 20:16:43 +0000

    Change some default files to reflext updates

--- xshop/defaults/xshop_test-default.py
+++ xshop/defaults/xshop_test-default.py
@@ -2,13 +2,11 @@
 # Define the code which runs inside the test containers. 
 #
 # Use the run() function to list which hooks should run in which containers
-# and in what order. Simply call H.run() as many times as needed. 
-#
-# Hooks can be any function in this file. 
+# and in what order. Simply call run() as many times as needed. 
 #
 
-def run(H):
-	H.run('attacker','run_exploit')
+def run(run):
+	run('attacker','run_exploit')
 
 def run_exploit():
 	return 0
